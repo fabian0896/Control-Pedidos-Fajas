a51bb6a610b19733a739d89a07e0b39474968fe3
Fabian David <fabian0896@outlook.com>
2018-10-07 13:04:34 -0500

    actualizacion menor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCI98pKlyR1UFCWuRjEdZ1ecwW1ym6ApoU",
+    authDomain: "pedidosfajas-57a6f.firebaseapp.com",
+    databaseURL: "https://pedidosfajas-57a6f.firebaseio.com",
+    projectId: "pedidosfajas-57a6f",
+    storageBucket: "pedidosfajas-57a6f.appspot.com",
+    messagingSenderId: "510872203578"
+  },
+  algolia:{
+    appId: 'R6CV7UD2A3',
+    apikey: 'f675b9c6ffef68ac45ec7364734b0938'
+  }
 };
